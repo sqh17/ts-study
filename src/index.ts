f8d455c4ac498cbcdbb7631957777ba711234992
sqh17 <852104785@qq.com>
2020-07-28 18:24:03 +0800

    原始数据理解1

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,27 @@ function greeter(person:string) {
   return "Hello, " + person
 }
 
-const user = "Jane User"
+
+let isDone: boolean = false;
+let createdByNewBoolean: boolean = Boolean(1);
+
+let decLiteral:number = 6;
+const hexLiteral: number = 0xf00d;
+const binaryLiteral: number = 0b1010;
+const octalLiteral: number = 0o744;
+
+const book: string = '深入浅出 Typescript'
+const user:string = "Jane User"
+
+const a:void = undefined;
+const b:null = null;
+const c:undefined = undefined;
+
+const symbol_1:symbol = Symbol(2);
+const symbol_2:symbol = Symbol(2);
+
+console.log(symbol_1 == symbol_2);
+
+// const bigInt_1:bigint = BigInt(Number.MAX_SAFE_INTEGER);
+enum Enum {A,B,C }
+console.log(Enum); 
